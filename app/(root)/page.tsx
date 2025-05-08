7d6de5bb7330008aee2c753680f1a43f5b749a3c
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-08 20:55:35 +0500

    Add RootLayout and Home page components for initial layout structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="h-[100dvh] w-full py-20 px-20 flex items-center justify-center text-3xl font-mona-sans ">
+      Home page
+    </div>
+  );
+};
+
+export default page;
