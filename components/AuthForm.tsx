3f31b0eb0c423dfca07529ba8e19225572666b24
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-09 21:28:36 +0500

    Add AuthForm component for user authentication with sign-in and sign-up functionality, including form validation and navigation handling.

---
+++ components/AuthForm.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name:
+      type === "sign-up" ? z.string().min(4).max(50) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(4),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const formSchema = authFormSchema(type);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successfully please sign in");
+        router.push("/sign-in");
+        console.log("Account created successfully", values);
+      } else {
+        toast.success("Logged in successfully");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`Something went wrong ${error}`);
+    }
+  }
+
+  const isSignIn = type === "sign-in";
+  return (
+    <div className="lg:min-w-[566px] card-border">
+      <div className="card  py-14 px-10 flex flex-col items-center justify-center gap-6">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <span className="text-primary-100">PerpWise</span>
+        </div>
+
+        <h3>Prepare job interview with Ai</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name={"name"}
+                label="Name"
+                placeholder="Your Name"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Your email address"
+              type="email"
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your Password"
+              type="password"
+            />
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign in" : "Create an Account"}
+            </Button>
+          </form>
+        </Form>
+        <p className="text-center">
+          {isSignIn ? "Don't have an account?" : "Already have an account?"}{" "}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {isSignIn ? "Sign up" : "Sign in"}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
