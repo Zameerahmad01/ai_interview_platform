d8f0fd661f2e9f93d6a69a4beb464a461564aee7
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-10 20:59:40 +0500

    Add InterviewCard component for displaying interview details, including type, role, date, and feedback options, enhancing the user interface for interview management.

---
+++ components/InterviewCard.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { getRandomInterviewCover } from "@/lib/utils";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const InterviewCard = ({
+  interviewId,
+  userId,
+  type,
+  role,
+  techstack,
+  finalized,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM D, YYYY");
+
+  return (
+    <div className="card-border min-w-[360px] min-h-96: max-sm:w-full">
+      <div className="card-interview">
+        <div>
+          <div className="absolute top-0 right-0 px-4 py-2 w-fit bg-light-600 rounded-bl-lg ">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+
+          <Image
+            src={getRandomInterviewCover()}
+            alt="cover"
+            height={90}
+            width={90}
+            className="rounded-full object-fit size-[90px]"
+          />
+
+          <h3 className="mt-5 capitalize">{role} interview</h3>
+
+          <div className="flex flex-row gap-5 mt-3">
+            <div className="flex flex-row gap-2 ">
+              <Image
+                src="/calendar.svg"
+                alt="calender"
+                width={22}
+                height={22}
+              />
+              <p>{formattedDate}</p>
+            </div>
+
+            <div className="flex flex-row gap-2 items-center ">
+              <Image src="/star.svg" alt="star" width={22} height={22} />
+
+              <p>{feedback?.totalScore || "---"}/100</p>
+            </div>
+          </div>
+
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment ||
+              "You haven't taken any interview yet. Take it now to improve your skill"}
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <p>techicons</p>
+
+          <Button className="btn-primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "View Feedback" : "View Interview"}
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterviewCard;
