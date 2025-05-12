0ff48cdaff22603012c856492b3dc5459cacf72e
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-12 22:15:52 +0500

    Add authentication actions for user sign-up, sign-in, and session management, integrating Firestore and Firebase Authentication for user handling.

---
+++ lib/actions/auth.actions.ts
@@ -0,0 +1,114 @@
+"use server";
+
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User is already exists. Please sign in instead.",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user ", error);
+
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use.",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create user.",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User not found. Please sign up.",
+      };
+    }
+
+    await setSessionCookie(idToken);
+  } catch (error) {
+    console.error("Error signing in user ", error);
+    return {
+      success: false,
+      message: "Failed to sign in user.",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: 60 * 60 * 24 * 7 * 1000,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 7,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) {
+    return null;
+  }
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.error("Error verifying session cookie ", error);
+    return null;
+  }
+}
+
+export async function isUserAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
+}
