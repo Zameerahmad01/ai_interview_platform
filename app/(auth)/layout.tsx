da6941404c4cedf8ca65edec76c9d2bf4632597d
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-12 22:40:33 +0500

    Add Firebase client configuration and initialization for Firestore and Authentication services, enabling backend functionality for the application.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,12 @@
+import { isUserAuthenticated } from "@/lib/actions/auth.actions";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isAuthenticated = await isUserAuthenticated();
+  if (isAuthenticated) {
+    redirect("/");
+  }
   return <div className="auth-layout">{children}</div>;
 };
 
