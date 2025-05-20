c2c4b38a306ffd9abb5dc0af8d4ead21b58adb0b
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-05-20 22:17:50 +0500

    Add Interview page component with Agent integration for interview generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h3>InterView Generation</h3>
+      <Agent userName="you" userID="user1" type="generation" />
+    </>
+  );
+};
+
+export default Page;
